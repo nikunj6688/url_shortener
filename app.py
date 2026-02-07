5c1ab867ae64dad880a1d0d705aaadad7d024ed9
Nikunj <nikunjgithub@example.com>
2026-02-07 17:47:59 +0530

    Fixed port for Render

--- app.py
+++ app.py
@@ -42,5 +42,8 @@ def redirect_url(short_code):
         return "URL not found"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
