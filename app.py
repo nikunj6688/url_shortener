f4fe638fb35a158f296ebd27dad37e72d00059d3
Nikunj <nikunjgithub@example.com>
2026-02-07 16:28:06 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect
+import random
+import string
+
+app = Flask(__name__)
+
+
+url_store = {}
+
+
+def generate_short_code(length=4):
+    characters = string.ascii_letters + string.digits
+    code = ""
+
+    for i in range(length):
+        code += random.choice(characters)
+
+    return code
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        long_url = request.form["long_url"]
+
+        short_code = generate_short_code()
+
+        url_store[short_code] = long_url
+
+        short_url = f"http://127.0.0.1:5000/{short_code}"
+
+        return render_template("index.html", short_url=short_url)
+
+    return render_template("index.html")
+
+
+
+@app.route("/<short_code>")
+def redirect_url(short_code):
+    if short_code in url_store:
+        return redirect(url_store[short_code])
+    else:
+        return "URL not found"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
