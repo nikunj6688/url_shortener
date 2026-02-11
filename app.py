7dcf9ee588470c6d5fdfb9e1f20cb320a079d5c6
Nikunj <nikunjgithub@example.com>
2026-02-11 21:17:28 +0530

    Fix Render deployment (gunicorn)

--- app.py
+++ app.py
@@ -42,8 +42,9 @@ def redirect_url(short_code):
         return "URL not found"
 
 
+
+
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
