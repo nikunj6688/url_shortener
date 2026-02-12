2ef6fb98c7262e4dbf97e1314f3208eadab6dff8
Nikunj <nikunjgithub@example.com>
2026-02-12 13:57:11 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import string
 import random
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ def generate_short_code(length=6):
 @app.route("/", methods=["GET", "POST"])
 def home():
     short_url = None
+
     if request.method == "POST":
         long_url = request.form.get("url")
 
@@ -26,9 +28,12 @@ def home():
 @app.route("/<code>")
 def redirect_url(code):
     long_url = url_mapping.get(code)
+
     if long_url:
         return redirect(long_url)
-    return "URL not found"
+    return "URL not found", 404
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
